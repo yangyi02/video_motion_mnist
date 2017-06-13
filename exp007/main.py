25e22c32bca45dd457c563dadbdda5500dab6456
yangyi02 <yangyi02@gmail.com>
2017-06-13 15:38:52 -0700

    better visualization for exp007

--- exp007/main.py
+++ exp007/main.py
@@ -57,34 +57,57 @@ def train_supervised(args, model, images, m_dict, reverse_m_dict, m_kernel):
     return model
 
 
+def get_img_size(n_row, n_col, img_size):
+    height = n_row * img_size + (n_row - 1) * int(img_size/10)
+    width = n_col * img_size + (n_col - 1) * int(img_size/10)
+    return width, height
+
+
+def get_img_coordinate(row, col, img_size):
+    y1 = (row - 1) * img_size + (row - 1) * int(img_size/10)
+    y2 = y1 + img_size
+    x1 = (col - 1) * img_size + (col - 1) * int(img_size/10)
+    x2 = x1 + img_size
+    return x1, y1, x2, y2
+
+
 def visualize(im1, im2, im3, pred, pred_motion, gt_motion, m_range, reverse_m_dict):
+    img_size = im1.size(2)
+    width, height = get_img_size(2, 4, img_size)
+    img = numpy.ones((height, width, 3))
+
+    im1 = im1[0].cpu().data.numpy().transpose(1, 2, 0).repeat(3, 2)
+    x1, y1, x2, y2 = get_img_coordinate(1, 1, img_size)
+    img[y1:y2, x1:x2, :] = im1
+
+    im2 = im2[0].cpu().data.numpy().transpose(1, 2, 0).repeat(3, 2)
+    x1, y1, x2, y2 = get_img_coordinate(1, 2, img_size)
+    img[y1:y2, x1:x2, :] = im2
+
+    im3 = im3[0].cpu().data.numpy().transpose(1, 2, 0).repeat(3, 2)
+    x1, y1, x2, y2 = get_img_coordinate(1, 3, img_size)
+    img[y1:y2, x1:x2, :] = im3
+
+    pred = pred[0].cpu().data.numpy().transpose(1, 2, 0).repeat(3, 2)
+    pred[pred > 1] = 1
+    pred[pred < 0] = 0
+    x1, y1, x2, y2 = get_img_coordinate(2, 1, img_size)
+    img[y1:y2, x1:x2, :] = pred
+
+    im_diff = numpy.abs(pred - im3)
+    x1, y1, x2, y2 = get_img_coordinate(2, 2, img_size)
+    img[y1:y2, x1:x2, :] = im_diff
+
+    pred_motion = label2flow(pred_motion[0].cpu().data.numpy().squeeze(), m_range, reverse_m_dict)
+    x1, y1, x2, y2 = get_img_coordinate(2, 3, img_size)
+    img[y1:y2, x1:x2, :] = pred_motion
+
+    gt_motion = label2flow(gt_motion[0].cpu().data.numpy().squeeze(), m_range, reverse_m_dict)
+    x1, y1, x2, y2 = get_img_coordinate(2, 4, img_size)
+    img[y1:y2, x1:x2, :] = gt_motion
+
     plt.figure(1)
-    plt.subplot(2,4,1)
-    plt.imshow(im1[0].cpu().data.numpy().squeeze(), cmap='gray')
-    plt.axis('off')
-    plt.subplot(2,4,2)
-    plt.imshow(im2[0].cpu().data.numpy().squeeze(), cmap='gray')
-    plt.axis('off')
-    plt.subplot(2,4,3)
-    plt.imshow(im3[0].cpu().data.numpy().squeeze(), cmap='gray')
-    plt.axis('off')
-    plt.subplot(2,4,4)
-    gt_m = label2flow(gt_motion[0].cpu().data.numpy().squeeze(), m_range, reverse_m_dict)
-    plt.imshow(gt_m)
-    plt.axis('off')
-    plt.subplot(2,4,8)
-    pred_m = label2flow(pred_motion[0].cpu().data.numpy().squeeze(), m_range, reverse_m_dict)
-    plt.imshow(pred_m)
-    plt.axis('off')
-    plt.subplot(2,4,7)
-    pred_im = pred[0].cpu().data.numpy().squeeze()
-    pred_im[pred_im > 1] = 1
-    pred_im[pred_im < 0] = 0
-    plt.imshow(pred_im, cmap='gray')
-    plt.axis('off')
-    plt.subplot(2,4,5)
-    im_diff = torch.abs(pred - im3)
-    plt.imshow(im_diff[0].cpu().data.numpy().squeeze(), cmap='gray')
+    plt.imshow(img)
     plt.axis('off')
     plt.show()
 
