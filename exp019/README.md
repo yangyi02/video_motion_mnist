f4a30647195d52a66bb4c8d4fcd8e07b6be0739e
yangyi02 <yangyi02@gmail.com>
2017-06-14 15:19:47 -0700

    add exp019, use 3 frames to estimate middle frame

---
+++ exp019/README.md
@@ -0,0 +1,41 @@
+local motion using fully convolutional network to predict every pixel motion
+MNIST dataset
+MNIST digit has motion, background also has motion
+use 3 frames instead of 5 frames, the second frame is the supervision
+hence use both previous 1 frame and last 1 frame to predict the middle frame
+the potential benefit of this is to handle occluded pixel prediction, because if the motion is consistent, then pixels in previous and later frames can recover the middle frame 
+another potential benefit of this is the motion consistency because we only use 3 frames
+adding noise to MNIST background
+use L1 loss instead of L2 loss for unsupervised learning
+
+### Synthetic motion on synthetic images
+The images are randomly sampled from MNIST dataset.
+MNIST contains 50000 training images and 10000 testing images.
+Image resolution: 32x32x1.
+motion range = 1 corresponds to 9 motion classes.
+motion range = 2 corresponds to 25 motion classes.
+motion range = 3 corresponds to 49 motion classes.
+motion range = 5 corresponds to 121 motion classes.
+
+input: two frames (i.e. 32x32x4)
+output: two local motion (i.e. 32x32x10x2) and two disappear map (i.e. 32x32x1x2) and two attention map (i.e. 32x32x1x2)
+
+| Global motion | Testing Accuracy (%) |
+| ------------- | ----------- | ----------- |
+| motion range = 1, supervised 2 frames, UNet |  |
+| motion range = 2, supervised 2 frames, UNet |  |
+| motion range = 3, supervised 2 frames, UNet | 94 |
+| motion range = 5, supervised 2 frames, UNet |  |
+| motion range = 1, unsupervised 3 frames, UNet | |
+| motion range = 2, unsupervised 3 frames, UNet | |
+| motion range = 3, unsupervised 3 frames, UNet | |
+| motion range = 5, unsupervised 3 frames, UNet | |
+
+Motivation
+
+This experiment again test the performance on using only 3 frames to replace 5 frames.
+The potential benefit is the computational speed, particularly when the low level convolution filters will be shared.
+
+Take Home Message:
+
+
---
+++ exp019/main.py
@@ -0,0 +1,172 @@
+import os
+import numpy
+import cv2
+import logging
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.autograd import Variable
+import torch.nn.functional as F
+import matplotlib.pyplot as plt
+
+from learning_args import parse_args
+from data import generate_images, motion_dict, load_mnist
+from models import FullyConvNet, FullyConvResNet, UNet, UNetBidirection
+from visualize import visualize
+logging.basicConfig(format='[%(levelname)s %(asctime)s %(filename)s:%(lineno)s] %(message)s',
+                            level=logging.INFO)
+
+
+def validate(args, model, images, m_dict, reverse_m_dict, m_kernel, best_test_acc):
+    test_acc = test_unsupervised(args, model, images, m_dict, reverse_m_dict, m_kernel)
+    if test_acc >= best_test_acc:
+        logging.info('model save to %s', os.path.join(args.save_dir, 'final.pth'))
+        with open(os.path.join(args.save_dir, 'final.pth'), 'w') as handle:
+            torch.save(model.state_dict(), handle)
+        best_test_acc = test_acc
+    logging.info('current best accuracy: %.2f', best_test_acc)
+    return best_test_acc
+
+
+def train_supervised(args, model, images, m_dict, reverse_m_dict, m_kernel):
+    m_range = args.motion_range
+    optimizer = optim.Adam(model.parameters(), lr=args.learning_rate)
+    best_test_acc = 0
+    train_loss = []
+    for epoch in range(args.train_epoch):
+        optimizer.zero_grad()
+        im1, im2, im3, gt_motion_f, gt_motion_b = generate_images(args, images, m_dict, reverse_m_dict)
+        im1 = Variable(torch.from_numpy(im1).float())
+        im2 = Variable(torch.from_numpy(im2).float())
+        im3 = Variable(torch.from_numpy(im3).float())
+        ones = Variable(torch.ones(im3.size(0), 1, im3.size(2), im3.size(3)))
+        gt_motion_f = Variable(torch.from_numpy(gt_motion_f))
+        gt_motion_b = Variable(torch.from_numpy(gt_motion_b))
+        if torch.cuda.is_available():
+            im1, im2, im3 = im1.cuda(), im2.cuda(), im3.cuda()
+            ones = ones.cuda()
+            gt_motion_f, gt_motion_b = gt_motion_f.cuda(), gt_motion_b.cuda()
+        pred, pred_f, motion_f, disappear_f, pred_b, motion_b, disappear_b, attn_f, attn_b  = model(im1, im3, ones)
+        motion_f = motion_f.transpose(1, 2).transpose(2, 3).contiguous().view(-1, model.n_class)
+        gt_motion_f = gt_motion_f.contiguous().view(-1)
+        motion_b = motion_b.transpose(1, 2).transpose(2, 3).contiguous().view(-1, model.n_class)
+        gt_motion_b = gt_motion_b.contiguous().view(-1)
+        loss_f = F.cross_entropy(motion_f, gt_motion_f)
+        loss_b = F.cross_entropy(motion_b, gt_motion_b)
+        loss = loss_f + loss_b
+        loss.backward()
+        optimizer.step()
+        train_loss.append(loss.data[0])
+        if len(train_loss) > 1000:
+            train_loss.pop(0)
+        ave_loss = sum(train_loss) / float(len(train_loss))
+        logging.info('epoch %d, training loss: %.2f, average training loss: %.2f', epoch, loss.data[0], ave_loss)
+        if (epoch+1) % args.test_interval == 0:
+            logging.info('epoch %d, testing', epoch)
+            best_test_acc = validate(args, model, images, m_dict, reverse_m_dict, m_kernel, best_test_acc)
+    return model
+
+
+def construct_image(im, motion, m_range, m_kernel, padding=0):
+    mask = F.softmax(motion)
+    im_expand = im.expand_as(mask) * mask
+    height = im.size(2) - 2 * m_range + 2 * padding
+    width = im.size(3) - 2 * m_range + 2 * padding
+    pred = Variable(torch.Tensor(im.size(0), im.size(1), height, width))
+    if torch.cuda.is_available():
+        pred = pred.cuda()
+    for i in range(im.size(0)):
+        pred[i, :, :, :] = F.conv2d(im_expand[i, :, :, :].unsqueeze(0), m_kernel, None, 1, padding)
+    return pred
+
+
+def train_unsupervised(args, model, images, m_dict, reverse_m_dict, m_kernel):
+    m_range = args.motion_range
+    optimizer = optim.Adam(model.parameters(), lr=args.learning_rate)
+    best_test_acc = 0
+    train_loss = []
+    for epoch in range(args.train_epoch):
+        optimizer.zero_grad()
+        im1, im2, im3, gt_motion_f, gt_motion_b = generate_images(args, images, m_dict, reverse_m_dict)
+        im1 = Variable(torch.from_numpy(im1).float())
+        im2 = Variable(torch.from_numpy(im2).float())
+        im3 = Variable(torch.from_numpy(im3).float())
+        ones = Variable(torch.ones(im3.size(0), 1, im3.size(2), im3.size(3)))
+        gt_motion_f = Variable(torch.from_numpy(gt_motion_f))
+        gt_motion_b = Variable(torch.from_numpy(gt_motion_b))
+        if torch.cuda.is_available():
+            im1, im2, im3 = im1.cuda(), im2.cuda(), im3.cuda()
+            ones = ones.cuda()
+            gt_motion_f, gt_motion_b = gt_motion_f.cuda(), gt_motion_b.cuda()
+        pred, pred_f, motion_f, disappear_f, pred_b, motion_b, disappear_b, attn_f, attn_b = model(im1, im3, ones)
+        gt = im2
+        loss_f = torch.abs(pred_f - gt).sum()
+        loss_b = torch.abs(pred_b - gt).sum()
+        loss = torch.abs(pred - gt).sum()
+        loss = loss + loss_f + loss_b
+        loss.backward()
+        optimizer.step()
+        train_loss.append(loss.data[0])
+        if len(train_loss) > 1000:
+            train_loss.pop(0)
+        ave_loss = sum(train_loss) / float(len(train_loss))
+        logging.info('epoch %d, training loss: %.2f, average training loss: %.2f', epoch, loss.data[0], ave_loss)
+        if (epoch+1) % args.test_interval == 0:
+            logging.info('epoch %d, testing', epoch)
+            best_test_acc = validate(args, model, images, m_dict, reverse_m_dict, m_kernel, best_test_acc)
+    return model
+
+
+def test_unsupervised(args, model, images, m_dict, reverse_m_dict, m_kernel):
+    m_range = args.motion_range
+    test_accuracy = []
+    for epoch in range(args.test_epoch):
+        im1, im2, im3, gt_motion_f, gt_motion_b = generate_images(args, images, m_dict, reverse_m_dict)
+        im1 = Variable(torch.from_numpy(im1).float())
+        im2 = Variable(torch.from_numpy(im2).float())
+        im3 = Variable(torch.from_numpy(im3).float())
+        ones = Variable(torch.ones(im3.size(0), 1, im3.size(2), im3.size(3)))
+        gt_motion_f = Variable(torch.from_numpy(gt_motion_f))
+        gt_motion_b = Variable(torch.from_numpy(gt_motion_b))
+        if torch.cuda.is_available():
+            im1, im2, im3 = im1.cuda(), im2.cuda(), im3.cuda()
+            ones = ones.cuda()
+            gt_motion_f, gt_motion_b = gt_motion_f.cuda(), gt_motion_b.cuda()
+        pred, pred_f, motion_f, disappear_f, pred_b, motion_b, disappear_b, attn_f, attn_b = model(im1, im3, ones)
+        pred_motion_f = motion_f.max(1)[1]
+        pred_motion_b = motion_b.max(1)[1]
+        if args.display:
+            m_range = args.motion_range
+            visualize(im1, im2, im3, pred, pred_motion_f, gt_motion_f, disappear_f, attn_f, pred_motion_b, gt_motion_b, disappear_b, attn_b, m_range, m_dict, reverse_m_dict)
+        accuracy_f = pred_motion_f.eq(gt_motion_f).float().sum() * 1.0 / gt_motion_f.numel()
+        accuracy_b = pred_motion_b.eq(gt_motion_b).float().sum() * 1.0 / gt_motion_b.numel()
+        test_accuracy.append(accuracy_f.cpu().data[0])
+        test_accuracy.append(accuracy_b.cpu().data[0])
+    test_accuracy = numpy.mean(numpy.asarray(test_accuracy))
+    logging.info('average testing accuracy: %.2f', test_accuracy)
+    return test_accuracy
+
+
+def main():
+    args = parse_args()
+    logging.info(args)
+    m_dict, reverse_m_dict, m_kernel = motion_dict(args.motion_range)
+    m_kernel = Variable(torch.from_numpy(m_kernel).float())
+    if torch.cuda.is_available():
+        m_kernel = m_kernel.cuda()
+    train_images, test_images = load_mnist()
+    [_, im_channel, args.image_size, _] = train_images.shape
+    model = UNetBidirection(args.image_size, im_channel, len(m_dict), args.motion_range, m_kernel)
+    if torch.cuda.is_available():
+        model = model.cuda()
+    if args.train:
+        if args.method == 'supervised':
+            model = train_supervised(args, model, train_images, m_dict, reverse_m_dict, m_kernel)
+        else:
+            model = train_unsupervised(args, model, train_images, m_dict, reverse_m_dict, m_kernel)
+    if args.test:
+        model.load_state_dict(torch.load(args.init_model_path))
+        test_unsupervised(args, model, test_images, m_dict, reverse_m_dict, m_kernel)
+
+if __name__ == '__main__':
+    main()
